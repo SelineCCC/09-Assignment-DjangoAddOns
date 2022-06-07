e9885a3569678a779b15dd743cab97664096cfb3
SelineCCC <seline0504.chen@gmail.com>
2022-06-06 17:26:49 -0700

    initial

---
+++ blogging/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
+
+
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
